7376e7add408845e05cb96dc4c8d2d956d2b7b77
Thomas Mortagne <thomas.mortagne@gmail.com>
2015-10-02 14:25:12 +0200

    Should be fully ok this time, at last

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # This script was created to make Duplicity backups.
 # One full backup per week then incremental.
 
@@ -17,9 +17,8 @@ mysqldump --single-transaction --routines --events --triggers --add-drop-table -
 ## Duplicity
 
 # doing a monthly full backup (1W)
-echo $PASSPHRASE
-echo "duplicity --full-if-older-than 1W --include='/etc' --include='/var/lib/xwiki' --include='/var/backups/sql/mysql_databases.sql' --exclude='**' / pydrive://${GOOGLE_ID}@developer.gserviceaccount.com/backup/server"
-duplicity --full-if-older-than 1W --include='/etc' --include='/var/lib/xwiki' --include='/var/backups/sql/mysql_databases.sql' --exclude='**' / pydrive://${GOOGLE_ID}@developer.gserviceaccount.com/backup/server
+echo "duplicity --full-if-older-than 1W --include='/etc' --include='/var/lib/xwiki' --include='/var/backups/sql/mysql_databases.sql' --exclude='**' / pydrive://$GOOGLE_ID@developer.gserviceaccount.com/backup/server"
+duplicity --full-if-older-than 1W --include='/etc' --include='/var/lib/xwiki' --include='/var/backups/sql/mysql_databases.sql' --exclude='**' / pydrive://$GOOGLE_ID@developer.gserviceaccount.com/backup/server
 
 # cleaning the remote backup space (deleting backups older than 1 month)
 echo "duplicity remove-older-than 1M --force pydrive://${GOOGLE_ID}@developer.gserviceaccount.com/backup/server"
