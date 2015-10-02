daf6694b896e01f01a7a07dc9aa61fc40b0e6957
Thomas Mortagne <thomas.mortagne@gmail.com>
2015-10-02 14:15:10 +0200

    More debug

--- backup.sh
+++ backup.sh
@@ -17,11 +17,12 @@ mysqldump --single-transaction --routines --events --triggers --add-drop-table -
 ## Duplicity
 
 # doing a monthly full backup (1W)
-echo 'duplicity --full-if-older-than 1W --include="/etc" --include="/var/lib/xwiki" --include="/var/backups/sql/mysql_databases.sql" --exclude="**" / pydrive://${GOOGLE_ID}@developer.gserviceaccount.com/backup/server'
-duplicity --full-if-older-than 1W --include="/etc" --include="/var/lib/xwiki" --include="/var/backups/sql/mysql_databases.sql" --exclude="**" / pydrive://${GOOGLE_ID}@developer.gserviceaccount.com/backup/server
+echo $PASSPHRASE
+echo "duplicity --full-if-older-than 1W --include='/etc' --include='/var/lib/xwiki' --include='/var/backups/sql/mysql_databases.sql' --exclude='**' / pydrive://${GOOGLE_ID}@developer.gserviceaccount.com/backup/server"
+duplicity --full-if-older-than 1W --include='/etc' --include='/var/lib/xwiki' --include='/var/backups/sql/mysql_databases.sql' --exclude='**' / pydrive://${GOOGLE_ID}@developer.gserviceaccount.com/backup/server
 
 # cleaning the remote backup space (deleting backups older than 1 month)
-echo 'duplicity remove-older-than 1M --force pydrive://${GOOGLE_ID}@developer.gserviceaccount.com/backup/server'
+echo "duplicity remove-older-than 1M --force pydrive://${GOOGLE_ID}@developer.gserviceaccount.com/backup/server"
 duplicity remove-older-than 1M --force pydrive://${GOOGLE_ID}@developer.gserviceaccount.com/backup/server
 
 # Unsetting the confidential variables
